149717971e08fe5863f8c06c5848b36781eaceac
Bipin Shrestha <bxs1216@mavs.uta.edu>
2022-04-11 22:20:53 -0500

    full_credit

--- P09/full_credit/color.cpp
+++ P09/full_credit/color.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "color.h"
 
 Color::Color(int red, int green, int blue)
--- P09/full_credit/main.cpp
+++ P09/full_credit/main.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 
 int main() {
-   Color red{255,0,0};
-   std::cout << red.colorize("Red") << std::endl;
+    int x,y,z;
+	Color orange{255,165,0};
+	std::cout << orange.colorize("UTA Orange")<<" ";
+	Color purple{159,43,104};
+	std::cout << purple.colorize("UTA purple")<<" ";
+	Color white{255,255,255};
+	std::cout << white.colorize("UTA White") << std::endl;
+	std::cout << "Enter red, green, and blue ints  ";
+	std::cin >>x;
+	std::cin >>y;
+	std::cin >>z;
+	Color demo{x,y,z};
+	std::cout << demo.colorize(demo.to_string())<<std::endl;
 }
